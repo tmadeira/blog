d0f13a9fe93d7c43449f9ec6d98cf7c3360266f1
Tiago Madeira <tmadeira@gmail.com>
2017-12-03 23:13:16 -0200

    Render LaTeX in .tex-render.

--- src/js/katex.js
+++ src/js/katex.js
@@ -5,4 +5,9 @@ import renderMathInElement from './katex-contrib/auto-render';
 
 export default function setupKatex() {
   renderMathInElement(document.body);
+
+  const spans = document.querySelectorAll('.tex-render');
+  for (let i = 0; i < spans.length; i++) {
+    katex.render(spans[i].innerHTML, spans[i]);
+  }
 }

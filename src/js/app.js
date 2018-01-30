116e7f6c3e16d0fd7cdd80c9e8b039de7b165198
Tiago Madeira <tmadeira@gmail.com>
2018-01-30 20:48:48 -0200

    Add comments layout.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import setupComments from './comments';
 import setupKatex from './katex';
 
+setupComments();
 setupKatex();
---
+++ src/js/comments.js
@@ -0,0 +1,39 @@
+const reply = function(event) {
+  event.preventDefault();
+
+  const form = document.getElementById('comment-form');
+  form.parentNode.removeChild(form);
+
+  const container = event.target.parentNode;
+  container.classList.add('replying');
+  container.appendChild(form);
+  form.querySelector('#name').focus();
+
+  document.getElementById('replyTo').value = event.target.getAttribute('rel');
+};
+
+const cancel = function(event) {
+  event.preventDefault();
+
+  const container = event.target.parentNode;
+  const form = document.getElementById('comment-form');
+  container.classList.remove('replying');
+  container.removeChild(form);
+
+  const comments = document.getElementById('comments');
+  comments.parentNode.insertBefore(form, comments.nextSibling);
+
+  document.getElementById('replyTo').value = '';
+}
+
+export default function setupComments() {
+  const replyButtons = document.querySelectorAll('.reply-button');
+  for (let i = 0; i < replyButtons.length; i++) {
+    replyButtons[i].addEventListener('click', reply);
+  }
+
+  const cancelButtons = document.querySelectorAll('.reply-cancel');
+  for (let i = 0; i < cancelButtons.length; i++) {
+    cancelButtons[i].addEventListener('click', cancel);
+  }
+}

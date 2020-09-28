d7b6a795af7cdb5040297e779594b2dc4612d6a3
Tiago Madeira <tmadeira@gmail.com>
2020-09-28 18:40:11 -0300

    Improve code syntax highlighting style.

--- components/Code.js
+++ components/Code.js
@@ -1,8 +1,9 @@
 import {Prism} from 'react-syntax-highlighter';
+import {vscDarkPlus} from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export default function Code({language, value}) {
   return (
-    <Prism language={language}>
+    <Prism language={language} style={vscDarkPlus}>
       {value}
     </Prism>
   );

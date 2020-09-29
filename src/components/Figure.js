4dce5700852d4f1124f2951fd40a3830b2fa66d4
Tiago Madeira <tmadeira@gmail.com>
2020-09-29 12:35:43 -0300

    Remove unused Figure and Markdown components.

--- src/components/Figure.js
+++
@@ -1,8 +0,0 @@
-export default function Figure({ alt, src }) {
-  return (
-    <figure>
-      <img src={src} alt={alt} />
-      {alt ? <figcaption>{alt}</figcaption> : null}
-    </figure>
-  );
-}
